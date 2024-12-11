9fbe975004d1f9af47d3398d3a0103ee8777fb20
MarkTauber <96663117+MarkTauber@users.noreply.github.com>
2024-12-11 12:14:47 +0300

    Update test_false.bat

--- test_false.bat
+++ test_false.bat
@@ -6,13 +6,14 @@ chcp 65001 >nul
 echo Проверка прав доступа к файлам процессов
 for /f "tokens=2 delims==" %%x in ('wmic process list full ^| find /i "executablepath" ^| find /i /v "system32" ^| find ":"') do (
     for /f eol^=^"^ delims^=^" %%z in ('echo.%%x') do (
+        echo. %%z 
         icacls "%%z" 2>nul | findstr /i "(F) (M) (W) :\\" | findstr /i ":\\ everyone authenticated users todos %username%"
-        if %ERRORLEVEL% EQU 0 echo. %%z >> "./Процессы_кратк".txt 
+        
     )
 )
 for /f "tokens=2 delims==" %%x in ('wmic process list full^|find /i "executablepath"^|find /i /v "system32"^|find ":"') do (
     for /f "tokens=* delims=" %%y in ('echo.%%x') do (
+        echo %%~dpy
         icacls "%%~dpy\" 2>nul | findstr /i "(F) (M) (W) :\\" | findstr /i ":\\ everyone authenticated users todos %username%" 
-        if %errorlevel% EQU 0  echo %%~dpy >> "./Процессы2_кратк".txt 
     )
 )
